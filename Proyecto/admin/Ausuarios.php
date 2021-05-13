13dfb22d2f63122ffb8c47a249d7c90fb43cfd67
edu545212 <edugamerhdcanal@gmail.com>
2021-05-13 10:06:30 +0200

    archivos admin

--- Proyecto/admin/Ausuarios.php
+++ Proyecto/admin/Ausuarios.php
@@ -10,7 +10,7 @@
     <h1>Usuario</h1>
     
     <div>
-        <a class="btn btn-success" href="./NuevoVC.php"> Nuevo <i class="fa fa-plus"></i></a>
+        <a class="btn btn-success" href="./NuevoU.php"> Nuevo <i class="fa fa-plus"></i></a>
     </div>
     <br>
 
