58906d04d149942f264c60f3be2490c5c4580a92
edu545212 <edugamerhdcanal@gmail.com>
2021-05-24 00:00:44 +0200

    TarjetasGraficas

---
+++ Proyecto/js/BusTarjetasGraficas.js
@@ -0,0 +1,26 @@
+$(buscarDatosTarjetasGraficas());
+
+function buscarDatosTarjetasGraficas(consulta){
+	$.ajax({
+		url: './admin/buscarTarjetasGraficas.php' ,
+		type: 'POST' ,
+		dataType: 'html',
+		data: {consulta: consulta},
+	})
+	.done(function(respuesta){
+		$("#datosTarjetasGraficas").html(respuesta);
+	})
+	.fail(function(){
+		console.log("error");
+	});
+}
+
+
+$(document).on('keyup','#caja_busqueda_TarjetasGraficas', function(){
+	var valor = $(this).val();
+	if (valor != "") {
+		buscarDatosTarjetasGraficas(valor);
+	}else{
+		buscarDatosTarjetasGraficas();
+	}
+});
