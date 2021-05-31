8fc90c12e30b800761295d2e9217c879fb483624
edu545212 <edugamerhdcanal@gmail.com>
2021-05-31 13:22:03 +0200

    comparativas

--- Proyecto/CompProcesadores.php
+++ Proyecto/CompProcesadores.php
@@ -1,10 +1,4 @@
 <?php
-    session_start();
-    if (!$_SESSION['Rol']=="usuario" || !$_SESSION['Rol']=="admin" ){
-        //Si el usuario ya esta logueado
-        header ('Location: index.php');
-        exit;
-    }
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,7 +10,20 @@
 <body>
     <?php include './inc/nav.php'; ?>
         <main>
-
+            <?php
+                include './BD/DAOProcesador.php';
+                $product = new Procesador();	
+            ?>
+            <div class="MarcaSection">
+                <?php
+                $Marca = $product->getMarca();
+                foreach($Marca as $MarcaDetails){	
+                ?>
+                <div class="list-group-item checkbox">
+                <label><input type="checkbox" class="productDetail Marca" value="<?php echo $MarcaDetails["Marca"]; ?>"  > <?php echo $MarcaDetails["Marca"]; ?></label>
+                </div>
+                <?php }	?>
+            </div>
         </main>
     <?php include './inc/footer.php'; ?>
 </body>
