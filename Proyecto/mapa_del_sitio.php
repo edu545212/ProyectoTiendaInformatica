23edbc2f260a2c4bc5ea3976f12e660e2658f1b5
edu545212 <edugamerhdcanal@gmail.com>
2021-06-02 16:41:54 +0200

    mapa del sitio

--- Proyecto/mapa_del_sitio.php
+++ Proyecto/mapa_del_sitio.php
@@ -8,16 +8,29 @@
 <body>
     <?php include './inc/nav.php'; ?>
         <main>
-            <h3>
-                <a href="index.php">Index</a><br>
-                <a href="consolas.php">Consolas</a><br>
-                <a href="videojuegos.php">Videojuegos</a><br>
-                <a href="registro.php">Registro</a><br>
-                <a href="recuperar_contraseña.php">Recuperar contraseña</a><br>
-                <a href="login.php">Login</a><br>
-                <a href="carrito.php">Carrito</a><br>
-                <a href="ajustes_usuario.php">Ajustes usuario</a><br>
-            </h3>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                        <h3>Usurios e Index</h3>
+                        <a href="index.php">Index</a><br>
+                        <a href="Registro.php">Registro</a><br>
+                        <a href="login.php">Login</a><br>
+                    </div>
+                    <div class="col-sm">
+                        <h3>Comparativas</h3>
+                        <a href="CompProcesadores.php">Comparativas de Procesadores</a><br>
+                        <a href="CompTarjetasGraficas.php">Comparativas de Tarjetas Graficas</a><br>
+                    </div>
+                    <div class="col-sm">
+                        <h3>Catalogo</h3>
+                        <a href="Cajas.php">Cajas</a><br>
+                        <a href="DiscosDuros.php">Discos Duros</a><br>
+                        <a href="TarjetasGraficas.php">Tarjetas Graficas</a><br>
+                        <a href="PlacasBases.php">PlacasBases</a><br>
+                        <a href="Procesadores.php">Procesadores</a><br>
+                    </div>
+                </div>
+            </div>
         </main>
     <?php include './inc/footer.php'; ?>
 </body>
